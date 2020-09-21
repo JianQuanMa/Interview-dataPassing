05612b77260cfb19998f8f0b476f2b04b681d7a0
Jian Ma <jianquanma@gmail.com>
2020-09-21 19:15:25 -0400

    completing data passing using prepare for segue 'forward method'

---
+++ SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  Interview-dataPassing
+//
+//  Created by Jian Ma on 9/21/20.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var stringLabel: UILabel!
+    var string: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configure(){
+        if let string = string {
+            stringLabel.text = string
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ViewController.swift
+++ ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segue" {
+        if let inputString = textField.text {
+            let secondVC = segue.destination as! SecondViewController
+            secondVC.string = inputString
+        }
+        }
+    }
+    
 
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+    }
+    
 
+    
 }
 
